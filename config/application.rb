860b85c7a3566d1594cc9c99a0f7b5adb58f5909
stevegordon <tdtsgordon@gmail.com>
2018-07-13 00:38:39 -0700

    more logical fix

--- config/application.rb
+++ config/application.rb
@@ -25,8 +25,6 @@ module TrailMix
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-    # browserify fix
-    config.browserify_rails.commandline_options = "-t [ babelify --presets [ es2015 ] ]"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
