97747c22266ea71b314ee143fe037f5b33d20519
stevegordon <tdtsgordon@gmail.com>
2018-07-31 23:59:24 -0700

    styling and alert added

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,13 +1,18 @@
 module PagesHelper
   def trim_weather(weatherData)
     x = 0
+    weather = []
+    data = Hash.new
     while x < weatherData["cnt"]
-      if weatherData["list"][x]["dt_txt"].include? "15:00:00"
-      cleanWeather.push(weatherData["list"][x]["weather"][0]["icon"])
-      cleanWeather.push(weatherData["list"][x]["dt_txt"])
+      if weatherData["list"][x]["dt_txt"].include? "21:00:00"
+      data["temp"] = weatherData["list"][x]["main"]["temp"]
+      data["icon"] = weatherData["list"][x]["weather"][0]["icon"]
+      data["date"] = weatherData["list"][x]["dt_txt"]
+      weather.push(data)
+      data = {}
       end
       x+=1
     end
-    return cleanWeather
+    return weather
   end
 end
